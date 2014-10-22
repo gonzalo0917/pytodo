50a58ffe782855eed79090ce5de0e576c3ad64b9
gonzalojpv <gonzalojpv@openmailbox.org>
2014-10-21 20:28:49 -0500

    GP, setup postgresql with django

--- pytodo/settings.py
+++ pytodo/settings.py
@@ -58,8 +58,14 @@ WSGI_APPLICATION = 'pytodo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       # 'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'pytodo',
+       'USER': 'postgres',
+       'PASSWORD': 'chipi',
+       'HOST': 'localhost',
+       'PORT': '',
     }
 }
 
